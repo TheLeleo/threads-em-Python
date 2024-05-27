191aeed171703349b0a86ab1831515228b7c6915
Leandro de Oliveira Custódio <99210769+TheLeleo@users.noreply.github.com>
2024-05-27 20:46:42 -0300

    voltando o codigo para 2 processos

--- main.py
+++ main.py
@@ -16,22 +16,22 @@ def calculaArray(lista):
 # criando 2 threads com a função de calculaArray, inserindo os respectivos arrays
 processo1 = ArquiteturaThread(target=calculaArray,  args=(ARRAY1,))
 processo2 = ArquiteturaThread(target=calculaArray, args=(ARRAY2,))
-processo3 = ArquiteturaThread(target=calculaArray, args=(ARRAY2,))
+#processo3 = ArquiteturaThread(target=calculaArray, args=(ARRAY2,))
 
 # iniciando as threads
 processo1.start()
 processo2.start()
-processo3.start()
+#processo3.start()
 
 # armazenando os resultados
 resultado1 = processo1.getResult()
 resultado2 = processo2.getResult()
-resultado3 = processo2.getResult()
+#resultado3 = processo2.getResult()
 
 # Realizar as somas dos resultados obtidos pelas 2 threads
 resultFinal = resultado1 + resultado2
-resultFinal += resultado3
+#resultFinal += resultado3
 
 # Printar os resultados no terminal
-#print("soma da thread 1 (", resultado1, "), com a thread 2 (", resultado2, ") =", resultFinal)
-print("soma da thread 1 (", resultado1, "), com a thread 2 (", resultado2,") juntamente com a thread 3 (", resultado3, ") =", resultFinal)
+print("soma da thread 1 (", resultado1, "), com a thread 2 (", resultado2, ") =", resultFinal)
+#print("soma da thread 1 (", resultado1, "), com a thread 2 (", resultado2,") juntamente com a thread 3 (", resultado3, ") =", resultFinal)
