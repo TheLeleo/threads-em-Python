1e505efafd3789aaae9b2eed62ef7e8ab4300abe
Leandro de Oliveira Custódio <99210769+TheLeleo@users.noreply.github.com>
2024-05-27 17:16:33 -0300

    calculando os arrays e somando os resultados

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 from thread import ThreadWithReturnValue
 
 
-def retornaNome(name):
-    return name
+def calculaArray(lista):
+    res = 0
+    for i in range(len(lista)):
+        res += lista[i]
+    return res
 
 
-p1 = ThreadWithReturnValue(target=retornaNome,  args=("melis",))
+p1 = ThreadWithReturnValue(target=calculaArray,  args=([1, 2, 10, 20, 61],))
 
-p2 = ThreadWithReturnValue(target=retornaNome, args=("lelis",))
+p2 = ThreadWithReturnValue(target=calculaArray, args=([3, 6, 9, 17, 64],))
 
 p1.start()
 p2.start()
 result = p1.join()
 result2 = p2.join()
+resultFinal = result + result2
 
-print(result, " ", result2)
+print("soma da thread 1 '",result,"', com a thread 2 '",result2,"' =", resultFinal)
+
 
